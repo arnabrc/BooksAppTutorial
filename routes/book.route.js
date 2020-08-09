8c66018bdeb0bade8feaaa2e32cbad07e20d779b
Arnab <arnabroychowdhury6@gmail.com>
2020-08-09 11:47:42 +0530

    Commit06

--- routes/book.route.js
+++ routes/book.route.js
@@ -7,7 +7,6 @@ const book_controller = require('../controllers/book.controller');
 
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', book_controller.test);
-module.exports = router;
 
 //Create API
 router.post('/create', book_controller.book_create);
@@ -20,3 +19,5 @@ router.put('/:id/update', book_controller.book_update);
 
 //Delete API
 router.delete('/:id/delete', book_controller.book_delete);
+
+module.exports = router;
